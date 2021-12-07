9f513d0a733e07966cc281949132b60d9210a7da
Adrian Corazza <69801867+AdrianCo97@users.noreply.github.com>
2021-12-07 14:21:47 +0100

    Created a get and post method for the database.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import express from "express";
+const express = require("express");
+const Database = require("better-sqlite3");
 
 const app = express();
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-})
+app.use(express.json());
+
+const db = new Database('./backend/users.db');
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
+});
+
+app.get("/", (req, res) => {
+    let preparedStatement = db.prepare("SELECT * FROM users");
+
+    let result = preparedStatement.all();
+    res.send(result);
+});
+
+app.post("/", (req, res) => {
+    let firstname = req.body.firstname;
+    let lastname = req.body.lastname;
+
+    const insert = db.prepare("INSERT INTO users (firstname, lastname) VALUES(?,?)");
+    insert.run(firstname, lastname);
 })
+
+
+
